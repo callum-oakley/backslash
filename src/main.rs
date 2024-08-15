1dab7755d88036b6171a5088ed1d969a469b77a1
Callum Oakley <hello@callumoakley.net>
2024-08-15 22:12:54 +0100

    read multiple files

--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,15 @@ use bs::run;
 
 fn main() -> Result<()> {
     let args: Vec<String> = env::args().collect();
-    if args.len() != 2 {
-        bail!("Usage: bs FILE");
+    if args.len() < 2 {
+        bail!("Usage: bs FILES");
     }
 
     let mut term = String::new();
-    File::open(&args[1])?.read_to_string(&mut term)?;
+    for file in &args[1..] {
+        File::open(file)?.read_to_string(&mut term)?;
+        term.push('\n');
+    }
 
     let mut input = Vec::new();
     io::stdin().read_to_end(&mut input)?;
