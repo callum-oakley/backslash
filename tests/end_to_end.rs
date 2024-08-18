46d078c4a9620360c6db071ad4705c8c26adb8d6
Callum Oakley <hello@callumoakley.net>
2024-08-18 21:45:07 +0100

    eq?

--- tests/end_to_end.rs
+++ tests/end_to_end.rs
@@ -7,4 +7,31 @@ fn end_to_end() {
     assert_run("id", "hello world", "hello world");
     assert_run("rev", "hello world", "dlrow olleh");
     assert_run("map inc", "hello world", "ifmmp!xpsme");
+    assert_run(
+        r"
+          \input.
+            let x = head input in
+            let y = head (tail input) in
+            let t = head (tail (tail input)) in
+            let f = head (tail (tail (tail input))) in
+            let res = (eq? x y) t f in
+            cons res nil
+        ",
+        "00tf",
+        "t",
+    );
+    assert_run(
+        r"
+          \input.
+            let x = head input in
+            let y = head (tail input) in
+            let t = head (tail (tail input)) in
+            let f = head (tail (tail (tail input))) in
+            let res = (eq? x y) t f in
+            cons res nil
+        ",
+        "01tf",
+        "f",
+    );
+    // assert_run(r"\input.+ (head input) (head (tail input))", "01", "a");
 }
