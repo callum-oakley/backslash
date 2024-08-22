0be801d29bc77ca38099fe12c20fe86497f54f45
Callum Oakley <hello@callumoakley.net>
2024-08-22 20:35:59 +0100

    string sugar

--- src/parser.rs
+++ src/parser.rs
@@ -4,7 +4,9 @@ use anyhow::{bail, ensure, Context, Result};
 use lazy_static::lazy_static;
 use regex::{Match, Matches, Regex};
 
-use crate::{int, term::Term};
+use crate::{bytes, int, term::Term};
+
+const TOKENS: &str = r#"#.*|'(\\.|[^'])'|"(\\.|[^"])*"|[\\\.\(\);]|[^#'"\\\.\(\);\s]+"#;
 
 impl FromStr for Term {
     type Err = anyhow::Error;
@@ -16,7 +18,7 @@ impl FromStr for Term {
                     if let Some((i, _)) = scope.iter().rev().enumerate().find(|&(_, v)| x == v) {
                         Ok(Term::Var(i))
                     } else {
-                        bail!("unbound variable {x}");
+                        bail!("parser: unbound variable {x}");
                     }
                 }
                 Sugar::Abs(arg, body) => {
@@ -27,6 +29,7 @@ impl FromStr for Term {
                 }
                 Sugar::App(s, t) => Ok(Term::app(desugar(scope, s)?, desugar(scope, t)?)),
                 Sugar::Int(n) => Ok(int::encode(*n)),
+                Sugar::Bytes(bytes) => Ok(bytes::encode(bytes)),
             }
         }
 
@@ -38,9 +41,10 @@ impl FromStr for Term {
 #[derive(Clone)]
 enum Sugar<'a> {
     Var(&'a str),
-    Abs(&'a str, Box<Sugar<'a>>),
-    App(Box<Sugar<'a>>, Box<Sugar<'a>>),
+    Abs(&'a str, Box<Self>),
+    App(Box<Self>, Box<Self>),
     Int(i64),
+    Bytes(Vec<u8>),
 }
 
 fn abs<'a>(arg: &'a str, body: Sugar<'a>) -> Sugar<'a> {
@@ -53,14 +57,13 @@ fn app<'a>(s: Sugar<'a>, t: Sugar<'a>) -> Sugar<'a> {
 
 fn parse(input: &str) -> Result<Sugar> {
     lazy_static! {
-        static ref RE: Regex =
-            Regex::new(r"#.*|'(\\.|[^'])'|[\\\.\(\);]|[^#'\\\.\(\);\s]+").unwrap();
+        static ref TOKENS_RE: Regex = Regex::new(TOKENS).unwrap();
     }
 
-    let mut tokens = RE.find_iter(input).peekable();
+    let mut tokens = TOKENS_RE.find_iter(input).peekable();
     let term = parse_term(&mut tokens)?;
     if !is_eof(&mut tokens) {
-        bail!("unexpected '{}'", peek(&mut tokens)?);
+        bail!("parser: unexpected '{}'", peek(&mut tokens)?);
     }
     Ok(term)
 }
@@ -77,18 +80,19 @@ fn parse_term<'a>(tokens: &mut Peekable<Matches<'_, 'a>>) -> Result<Sugar<'a>> {
             ")" | ";" => {
                 break;
             }
-            "." | "=" => bail!("unexpected '{token}'"),
+            "." | "=" => bail!("parser: unexpected '{token}'"),
             "(" => parse_bracketed(tokens),
             r"\" => parse_abs(tokens),
             "let" => parse_let(tokens),
             token if is_int(token) => parse_int(tokens),
             token if is_char(token) => parse_char(tokens),
+            token if is_string(token) => parse_string(tokens),
             _ => parse_identifier(tokens).map(Sugar::Var),
         }?);
     }
 
     match terms.len() {
-        0 => bail!("empty term"),
+        0 => bail!("parser: empty term"),
         1 => Ok(terms.remove(0)),
         _ => {
             let term = terms.remove(0);
@@ -142,17 +146,41 @@ fn parse_char<'a>(tokens: &mut Peekable<Matches<'_, 'a>>) -> Result<Sugar<'a>> {
         b"'\\r'" => Ok(Sugar::Int(b'\r'.into())),
         b"'\\t'" => Ok(Sugar::Int(b'\t'.into())),
         [b'\'', c, b'\''] | [b'\'', b'\\', c, b'\''] => Ok(Sugar::Int((*c).into())),
-        _ => bail!("malformed char '{token}'"),
+        _ => bail!("parser: malformed char '{token}'"),
     }
+}
+
+fn parse_string<'a>(tokens: &mut Peekable<Matches<'_, 'a>>) -> Result<Sugar<'a>> {
+    let token = next(tokens)?.as_bytes();
+    assert_eq!(token[0], b'"');
+    assert_eq!(token[token.len() - 1], b'"');
+    let mut bytes = Vec::new();
+    let mut escape = false;
+    for &c in &token[1..token.len() - 1] {
+        if escape {
+            match c {
+                b'n' => bytes.push(b'\n'),
+                b'r' => bytes.push(b'\r'),
+                b't' => bytes.push(b'\t'),
+                c => bytes.push(c),
+            }
+            escape = false;
+        } else if c == b'\\' {
+            escape = true;
+        } else {
+            bytes.push(c);
+        }
+    }
+    Ok(Sugar::Bytes(bytes))
 }
 
 fn parse_identifier<'a>(tokens: &mut Peekable<Matches<'_, 'a>>) -> Result<&'a str> {
     let token = next(tokens)?;
     if matches!(token, r"\" | "." | "(" | ")" | "let" | "=" | ";") {
-        bail!("unexpected '{token}'");
+        bail!("parser: unexpected '{token}'");
     }
     if is_int(token) {
-        bail!("identifier can't be an int");
+        bail!("parser: identifier can't be an int");
     }
     Ok(token)
 }
@@ -165,19 +193,24 @@ fn is_char(s: &str) -> bool {
     s.as_bytes()[0] == b'\''
 }
 
+fn is_string(s: &str) -> bool {
+    s.as_bytes()[0] == b'"'
+}
+
 fn is_eof(tokens: &mut Peekable<Matches>) -> bool {
     tokens.peek().is_none()
 }
 
 fn peek<'a>(tokens: &mut Peekable<Matches<'_, 'a>>) -> Result<&'a str> {
-    tokens.peek().context("EOF").map(Match::as_str)
+    tokens.peek().context("parser: EOF").map(Match::as_str)
 }
 
 fn next<'a>(tokens: &mut Peekable<Matches<'_, 'a>>) -> Result<&'a str> {
-    tokens.next().context("EOF").map(|m| m.as_str())
+    tokens.next().context("parser: EOF").map(|m| m.as_str())
 }
 
 fn consume(expected: &str, tokens: &mut Peekable<Matches>) -> Result<()> {
-    ensure!(tokens.next().context("EOF")?.as_str() == expected);
+    let token = next(tokens)?;
+    ensure!(token == expected, "parser: unexpected '{token}'");
     Ok(())
 }
